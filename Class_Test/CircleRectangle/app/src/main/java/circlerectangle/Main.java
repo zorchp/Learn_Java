19c37274f0d92a54e88e3a4e67d33d79c3db423e
zorchp <zorchp@gmail.com>
2023-10-31 11:18:46 +0800

    add-gradle-proj-1

--- Class_Test/CircleRectangle/app/src/main/java/circlerectangle/Main.java
+++ Class_Test/CircleRectangle/app/src/main/java/circlerectangle/Main.java
@@ -3,5 +3,25 @@ public class Main {
     public static void main(String[] args) {
         Circle circle = new Circle(1);
         System.out.println("A circle " + circle.toString());
+        System.out.println("The color is " + circle.getColor());
+        System.out.println("The radius is " + circle.getRadius());
+        System.out.println("The area is " + circle.getArea());
+        System.out.println("The diameter is " + circle.getDiameter());
+
+        Rectangle rect = new Rectangle(3, 4);
+        System.out.println("\n A Rectangle is " + rect.toString());
+        System.out.println("The area is " + rect.getArea());
+        System.out.println("The perimeter is " + rect.getPerimeter());
+        // A circle created on Tue Oct 31 10:56:23 CST 2023
+        // color: white and filled: false
+        // The color is white
+        // The radius is 1.0
+        // The area is 3.141592653589793
+        // The diameter is 2.0
+
+        // A Rectangle is created on Tue Oct 31 10:56:23 CST 2023
+        // color: white and filled: false
+        // The area is 12.0
+        // The perimeter is 14.0
     }
 }
