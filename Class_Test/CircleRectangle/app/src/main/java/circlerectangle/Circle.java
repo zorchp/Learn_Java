20acfccaef79433fce69ccf2bb7ef238fd035f07
zorchp <zorchp@gmail.com>
2023-10-31 10:52:22 +0800

    add-gradle-proj

---
+++ Class_Test/CircleRectangle/app/src/main/java/circlerectangle/Circle.java
@@ -0,0 +1,41 @@
+
+public class Circle extends GeometricObj {
+    private double radius;
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+        this.radius = radius;
+        setColor(color);
+        setFilled(filled);
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+
+    public double getDiameter() {
+        return 2 * radius;
+    }
+
+    public double getPerimeter() {
+        return getDiameter() * Math.PI;
+    }
+
+    public void printCircle() {
+        System.out.println("Circle: " + getDateCreated() + " radius = " + radius);
+    }
+}
---
+++ Class_Test/CircleRectangle/app/src/main/java/circlerectangle/GeometricObj.java
@@ -0,0 +1,43 @@
+// package circlerectangle;
+
+import java.util.Date;
+
+public class GeometricObj {
+    private String color = "white";
+    private boolean filled;
+    private Date dateCreated;
+
+    public GeometricObj() {
+        dateCreated = new Date();
+    }
+
+    public GeometricObj(String color, boolean filled) {
+        dateCreated = new Date();
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public String toString() {
+        return "created on " + dateCreated + "\ncolor: " + color + " and filled: " + filled;
+    }
+}
